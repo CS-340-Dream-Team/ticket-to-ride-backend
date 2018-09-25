07282b4742dd4bad4d813a3b05a6a28727100631
Andrew <merrillman32@gmail.com>
2018-09-25 14:20:06 -0600

    basic setup of ServerCommunicator

---
+++ handlers/GamesHandler.ts
@@ -0,0 +1,8 @@
+import { BaseHandler } from "./BaseHandler";
+import { Request, Response } from "express";
+
+export class GamesHandler extends BaseHandler{
+    handle(req: Request): Response {
+        return;
+    }
+}
---
+++ handlers/LoginHandler.ts
@@ -0,0 +1,8 @@
+import { BaseHandler } from "./BaseHandler";
+import { Request, Response } from "express";
+
+export class LoginHandler extends BaseHandler{
+    handle(req: Request): Response {
+        return;
+    }
+}
