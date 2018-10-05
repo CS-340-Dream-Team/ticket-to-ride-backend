bf145b3c3ce9581026a4ab624a8756223f01ad4b
Carter McBride <ThatCarterMcBride@users.noreply.github.com>
2018-10-05 16:06:33 -0600

    Fix cors (#13)
    * Remove cors dependency
    
    * Re-add the cors dependency, correctly
    
    * Cache the OPTIONS preflight response for 10 mins

--- src/handlers/ServerCommunicator.ts
+++ src/handlers/ServerCommunicator.ts
@@ -25,7 +25,9 @@ export class ServerCommunicator {
     public app: express.Application;
 
     private config(): void {
-        this.app.use(cors);
+        // the number of seconds to cache the cors OPTIONS preflight request
+        const maxAge = 600;
+        this.app.use(cors({ maxAge }));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
